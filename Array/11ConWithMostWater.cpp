3da2c6a42851c75f2c56be34f68c1438cf31d0ee
Bipro Roy <roybipro896@gmail.com>
2025-05-05 08:46:44 +0600

    add a new file contain most water

---
+++ Array/11ConWithMostWater.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>  // for min and max functions
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0, right = height.size() - 1, maxarea = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int h = min(height[left], height[right]);
+            int area = width * h;
+            maxarea = max(maxarea, area);
+
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+
+        return maxarea;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> height = {1,8,6,2,5,4,8,3,7}; 
+    int result = sol.maxArea(height);
+    cout  << result << endl;
+    return 0;
+}
