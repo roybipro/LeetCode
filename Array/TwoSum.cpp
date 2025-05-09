05a51d1a1ffa57d233e8e69417460e4d91a2dce2
Bipro Roy <roybipro896@gmail.com>
2025-05-09 21:26:29 +0600

    edit the brutforce code}
    c

--- Array/TwoSum.cpp
+++ Array/TwoSum.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
 using namespace std;
 
-//brute force
-int twoSum(int arr[],int n,int target){
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-        int sum = arr[i]+arr[j];
-        if(sum==target){
-            cout<<i<<" "<<j<<endl;
-            return 0;
-
+// brute force
+int twoSumBB(int arr[], int n, int target)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (target == arr[i] + arr[j])
+            {
+                cout << i << " " << j << endl;
+                return 0;
+            }
         }
-        }
-
     }
     return 0;
 }
 
-int main() {
+int main()
+{
     // Your code goes here
-    int arr[]={2,3,5,6,7,4,7};
-    int n= sizeof(arr)/sizeof(arr[0]);
-    int target=9;
-    twoSum(arr,n,target);
+    int arr[] = {2, 3, 5, 6, 7, 4, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 9;
+    twoSumBB(arr, n, target);
     return 0;
 }
