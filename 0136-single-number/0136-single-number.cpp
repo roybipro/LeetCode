bba1801149c046486ab9326d09a0cb6879eca06d
Roy Bipro <roybipro896@gmail.com>
2026-02-04 20:12:34 +0600

    Time: 0 ms (100%), Space: 20.5 MB (82.64%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int xorr = 0;
-        for(int i=0;i<nums.size();i++){
-            xorr = xorr ^ nums[i];
+        int ans =0;
+        for(int val : nums){
+            ans = ans^val;
         }
-        return xorr;
+        return ans;
     }
 };
