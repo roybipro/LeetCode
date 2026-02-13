f2d0b7479df0b6fac57ee703efa4ff80b3bba63e
Roy Bipro <roybipro896@gmail.com>
2026-02-13 20:59:51 +0600

    Time: 0 ms (100%), Space: 7.7 MB (57.51%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string result = "";
+        
+        while (columnNumber > 0) {
+            columnNumber--;  // Adjust because A = 1
+            char ch = 'A' + (columnNumber % 26);
+            result = ch + result;
+            columnNumber /= 26;
+        }
+        
+        return result;
+    }
+};
