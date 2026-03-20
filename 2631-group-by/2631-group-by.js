149e5da703bffa155c85628182d14268d6d726d8
Roy Bipro <roybipro896@gmail.com>
2026-03-20 15:20:18 +0600

    Time: 127 ms (5.54%), Space: 81.1 MB (5.58%) - LeetHub

---
+++ 2631-group-by/2631-group-by.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    const result = {};
+
+    for (const item of this) {
+        const key = fn(item);
+
+        if (!result[key]) {
+            result[key] = [];
+        }
+
+        result[key].push(item);
+    }
+
+    return result;
+};
